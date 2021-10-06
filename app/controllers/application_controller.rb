99c2f9ced67842173d8cf309e900e3ccf6fb7a59
Luka Domitrovič <luka@domitrovic.si>
2021-10-06 23:43:40 +0200

    add set current cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_cart
+
+  def set_current_cart
+    @current_cart ||= Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @current_cart = Cart.create
+    session[:cart_id] = @current_cart.id
+  end
 end
