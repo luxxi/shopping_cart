ab6554588f05c6fb728215c75baac4ab93a18729
Luka Domitrovič <luka@domitrovic.si>
2021-10-07 22:17:39 +0200

    add update discounts activity and spec

---
+++ app/activities/shopping_cart/line_item/update_discounts_activity.rb
@@ -0,0 +1,47 @@
+module ShoppingCart
+  module LineItem
+    class UpdateDiscountsActivity
+      class << self
+        def call(cart:, line_item:, previous_quantity:)
+          new(line_item: line_item, previous_quantity: previous_quantity).call
+        end
+      end
+      
+      attr_accessor :line_item, :previous_quantity
+    
+      def initialize(line_item:, previous_quantity:)
+        @previous_quantity = previous_quantity
+        @line_item = line_item
+      end
+    
+      def call
+        offers.each do |offer|
+          offer.offerable.send(action, **offer_params)
+        end
+        line_item.reload
+      end
+
+      private
+      def offers
+        line_item.product.offers
+      end
+
+      def action
+        raise ArgumentError if line_item.quantity == previous_quantity
+        action ||= case
+        when line_item.quantity > previous_quantity
+          :apply
+        when line_item.quantity < previous_quantity
+          :remove
+        end
+      end
+
+      def offer_params
+        {
+          line_item: line_item,
+          previous_quantity: previous_quantity
+        }
+      end
+    end  
+  end
+end
---
+++ spec/activities/shopping_cart/line_item/update_discounts_activity_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe ShoppingCart::LineItem::UpdateDiscountsActivity do
+  describe '.call' do
+    let(:cart) { create(:cart) }
+    let(:line_item) { create(:line_item, cart: cart) }
+
+    context 'when arguments are provided' do
+      context 'previous quantity is lower than current quantity' do
+        subject do
+          described_class.call(
+            cart: cart,
+            line_item: line_item,
+            previous_quantity: line_item.quantity.pred
+          )
+        end
+
+        context 'when offer buy one get one free applied to product' do
+          before do
+            create(:offer, :buy_one_get_one_free, product: line_item.product)
+          end
+
+          it 'should apply discount' do        
+            expect_any_instance_of(Offer::BuyOneGetOneFree).to receive(:apply)
+            subject
+          end
+        end
+      end
+
+      context 'previous quantity is higher than current quantity' do
+        context 'when offer buy one get one free applied to product' do
+          before do
+            create(:offer, :buy_one_get_one_free, product: line_item.product)
+          end
+
+          subject do
+            described_class.call(
+              cart: cart,
+              line_item: line_item,
+              previous_quantity: line_item.quantity.succ
+            )
+          end
+
+          it 'should remove discount' do        
+            expect_any_instance_of(Offer::BuyOneGetOneFree).to receive(:remove)
+            subject
+          end
+        end
+      end
+    end
+
+    context 'when arguments are not provided' do
+      it 'raises ArgumentError exception' do
+        expect { described_class.call }.to raise_error(ArgumentError)
+      end
+    end
+
+  end
+end
