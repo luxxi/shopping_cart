4632ee68495d08cbe05522a8e14276cc8460d069
Luka Domitrovič <luka@domitrovic.si>
2021-10-09 09:06:29 +0200

    display offers for line item

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @products = Product.all
-    @line_items = @current_cart.line_items.order(:created_at)
+    @line_items = @current_cart.line_items.includes(:offers).order(:created_at)
   end
 end
